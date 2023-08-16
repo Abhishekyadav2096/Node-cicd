2c25b4a5247620e57c02d72d644efa99217c5f83
Abhishek Yadav <abhishek.yadav@harnstech.com>
2023-08-16 10:43:47 +0530

    node-jenkins api

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("ABHISHEK YADAV");
+});
+
+app.listen(4001, () => {
+  console.log(`Server Started at ${4001}`);
+});
